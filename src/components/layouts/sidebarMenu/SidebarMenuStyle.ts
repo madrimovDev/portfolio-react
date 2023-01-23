7f66394c035a85320f108aa5711e4e3894ec7e4c
madrimovDev <madrimov5014@gmail.com>
2023-01-23 18:01:53 +0500

    sidebar

--- src/components/layouts/sidebarMenu/SidebarMenuStyle.ts
+++ src/components/layouts/sidebarMenu/SidebarMenuStyle.ts
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled'
 
 const SidebarMenuStyle = styled('div')`
-  position: absolute;
-  width: 300px;
-  height: 100%;
-  background: ${({ theme }) => theme.colors.sidebarBgColor};
-  top: 0;
-  right: 0;
+	position: absolute;
+	width: 300px;
+	height: 100%;
+	background: ${({ theme }) => theme.colors.sidebarBgColor};
+	top: 0;
+	right: 0;
 `
 
 export default SidebarMenuStyle
