658bbef5ef1ea90878be5f15f484c49096681875
madrimovDev <madrimov5014@gmail.com>
2023-01-24 13:09:24 +0500

    useOutsideclick

--- src/components/layouts/sidebarMenu/SidebarMenu.tsx
+++ src/components/layouts/sidebarMenu/SidebarMenu.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 import SidebarMenuStyle, { MenuButtonStyle, SidebarHeader, SidebarWrapper } from './SidebarMenuStyle'
 
 import MenuOutlined from '@ant-design/icons/MenuOutlined'
 import CloseOutlined from '@ant-design/icons/CloseOutlined'
+import { useOutsideClick } from '@/hooks'
 
 const SidebarMenu = () => {
 	const [isShow, setIsShow] = useState<boolean>(false)
 	const toggleShow = () => setIsShow((prev) => !prev)
+	const ref = useRef<HTMLDivElement>(null)
+	
+	useOutsideClick(ref, () => setIsShow(false))
 
 	return (
-		<SidebarMenuStyle isShow={isShow}>
+		<SidebarMenuStyle
+			ref={ref}
+			isShow={isShow}>
 			<SidebarWrapper>
 				<SidebarHeader>
 					<MenuButtonStyle
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,1 @@
-export {}
+export { default as useOutsideClick } from './useOutsideClick'
